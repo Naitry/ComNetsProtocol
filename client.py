c6ab03df8daafc93760892f740420af28aceca2d
Naitry <77989202+Naitry@users.noreply.github.com>
2023-11-18 12:52:26 -0600

    adding multiple users

--- client.py
+++ client.py
@@ -4,36 +4,36 @@ import socket
 class SimpleClient:
     def __init__(self,
                  host: str = '127.0.0.1',
-                 port: int = 65433):
+                 port: int = 65443):
         self.host: str = host
         self.port: int = port
         self.on: bool = False
 
-    def handshake(self, sock: socket):
-        print("sending handshake...")
-        sock.sendall(bytes("connect", "utf-8"))
-        reply = sock.recv(1024)
+    def handshake(self, sock: socket.socket) -> bool:
+        print("Sending handshake...")
+        sock.sendall(b'PING')
+        reply: bytes = sock.recv(1024)
         return reply == b'PONG'
-    commands = {'terminate', 'otherCommand1', 'otherCommand2'}
 
-    def runClient(self):
-        # open socket client side
+    def run_client(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.host, self.port))
             self.on = self.handshake(s)
-            while (self.on):
-                send: str = input()
-                if (send != ''):
-                    if send in self.commands:
-                        if send == b'terminate':
-                            s.sendall(b'TERM')
+            while self.on:
+                send: str = input("Enter command or message: ")
+                if send:
+                    if send == 'terminate':
+                        s.sendall(b'TERM')
+                        s.close()
+                        break
                     else:
-                        s.sendall("MESG")
+                        s.sendall(('MESG ' + send).encode('utf-8'))
 
-                        data = s.recv(1024)
+                    data: bytes = s.recv(1024)
+                    if data:
                         print(f"Received from server: {data.decode()}")
 
 
 if __name__ == "__main__":
     client = SimpleClient()
-    client.runClient()
+    client.run_client()
